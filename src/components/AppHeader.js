3fe3bc29855d2150dc10a8cb47e0b2ff3e70bc5b
CanterburyEd23 <95231297+CanterburyEd23@users.noreply.github.com>
2022-02-18 17:47:37 +0000

    Update AppHeader.js

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from "react";
 const AppHeader = () => {
     return (
         <header className="appHeaderContainer">
-            <h1>Ed's Todo App</h1>
+            <h1>Ed's To-do App</h1>
             <p><i>A simple tool, to aid in organisation</i></p>
         </header>
     )
