13d7515b5d01dca3c40c516c2f9ddb062818aa0c
CanterburyEd23 <95231297+CanterburyEd23@users.noreply.github.com>
2022-02-18 17:47:19 +0000

    Update AppFooter.js

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -3,7 +3,7 @@ import React from "react";
 const AppFooter = () => {
     return (
         <footer className="appFooterContainer">
-            <h4>Ed's Todo App, Copyright 2022</h4>
+            <h4>Ed's To-do App, Copyright 2022</h4>
             <p>Built using React and Redux, with html, css, jsx, and javascript</p>
         </footer>
     )
